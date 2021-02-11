d83f063ccec480a10436d9b1df42b9f05ef131a4
Tintu Devaraj <tintudevaraj@gmail.com>
2021-02-12 01:46:50 +0400

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch
+from torch import nn
+from torch import tensor
+from torch import optim
+import torchvision.datasets as datasets,
+import torchvision.transforms as transforms
+import torchvision.models as models
+import PIL
+from PIL import Image
+from collections import OrderedDict
+import argparse
+import torch.nn.functional as F
+from torch.autograd import Variable
+import json
+from train import train
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--image',type = str,help='Point  to image file for prediction',required = True)
+parser.add_argument('--checkpoint',type = str,help='Point  to checkpoint file as str',required = True)
+parser.add_argument('--top_k',dest='topk',action='store',default=5,help='Choose top k matches as int')
+parser.add_argument('--category_names',dest = "category_names",action = "store",default = 'cat_to_name.json')
+parser.add_argument('--gpu',default=False,action="store",dest="gpu")
+    
+args = parser.parse_args()
+
+
+
+def load_checkpoint(path):
+    
+    checkpoint = torch.load(path)
+    
+    for param in model.parameters():
+        param.requires_grad = False
+        
+    model.class_to_idx = checkpoint[class_to_idx]
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
+
+def processimage(image):
+    pil_image = Image.open(image)
+    
+    process_image = transforms.Compose([transforms.Resize(256),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean=[0.485,0.456,0.406],
+                                                             std=[0.229,0.224,0.225])
+                                       ])
+    np_image = process_image(pil_image)
+    
+    return np_image
+
+def main():
+    
+    model = load_checkpoint('model_checkpoint.pth')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model.to(device)
+    image = process_image(image).to(device)
+    
+    np_image = image.unsqueeze_(0)
+    model.eval()
+
+    with torch.no_grad():
+       logps = model.forward(np_image)
+        ps = torch.exp(logps)
+        top_k,top_classes_idx = ps.topk(topk,dim=1)
+        top_k,top_classes_idx = np.array(top_k.to('cpu')[0]), np.array(top_classes_idx.to('cpu')[0])
+    
+        idx_to_class = {x:y for y,x in model.class_to_idx.items()}
+    
+        top_classes = []
+        for index in top_classes_idx:
+            top_classes.append(idx_to_class[index])
+        
+    
+
+    if args.category_names != None:
+        with open(args.category_names,'r')as f:
+            cat_to_name = json.load(f)
+            top_class_names = [cat_to_name[top] for top in list(top_classes)]
+            print(probabilities:{list(top_k)},classes:{ list(top_class_names)})
+        
+    else:
+        print(probabilities:{list(top_k)},classes:{ list(top_classes)})
+        
+        
+if _name_ == '_main_':
+    main()
+    
